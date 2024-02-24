f13d42c5a2b98ff9bffa19eb16e84b28559a1d49
fidaajarrar <fidaa.j152@gmail.com>
2024-02-24 23:10:21 +0200

    task3react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar/Navbar";
+import About from "./components/About/About";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <About />
+      <Footer />
+    </>
+  );
+}
+export default App
---
+++ src/components/About/About.jsx
@@ -0,0 +1,17 @@
+import {useState}  from 'react';
+
+function About() {
+
+  let [count,setCount] = useState(0);
+  const changeCount = ()=>{
+    setCount(count + 1);
+    console.log(count);
+  };
+  return (
+    <div>
+      <button onClick={changeCount}>Fidaa jarrar count : {count}</button>
+    </div>
+  )
+}
+
+export default About
